11ccbe7b3c07199b3d451e2f3e2a4ea03d21ca17
12den24@gmail.com <12den24@gmail.com>
2022-08-26 21:13:58 +0300

    Добавьте файлы проекта.

---
+++ Modul_2_Task_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Задание 2
+Дан двумерный массив размерностью 5×5, заполненный случайными числами из диапазона от –100 до 100. 
+Определить сумму элементов массива, расположенных между минимальным и максимальным элементами.
+.*/
+
+namespace Modul_2_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            int[,] twoDArray = new int[size, size];
+            Random random = new Random();
+
+            for (int i = 0; i < twoDArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoDArray.GetLength(1); j++)
+                {
+                    twoDArray[i, j] = random.Next(-100, 100);
+                    if (twoDArray[i, j] > 0 )
+                    {
+                        Console.Write($" {twoDArray[i, j]} ");
+                    }
+                    else
+                    {
+                        Console.Write($"{twoDArray[i, j]} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            var buf = twoDArray.Cast<int>().Select(c => c).ToArray();
+            int minInd = Array.IndexOf(buf, buf.Min());
+            Console.WriteLine($"MinInd = {minInd}");
+            
+            int maxInd = Array.IndexOf(buf, buf.Max());
+            Console.WriteLine($"MaxInd = {maxInd}");
+            Console.WriteLine();
+            foreach (var item in buf)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            int sum = 0;
+            if (minInd > maxInd)
+            {
+                int tmp = minInd;
+                minInd = maxInd;
+                maxInd = tmp;
+            }
+            for (int i = minInd; i < maxInd; i++)
+            {
+                sum += buf[i];
+            }
+            Console.WriteLine($"Сумма = {sum}");
+        }
+    }
+}
---
+++ Modul_2_Task_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Modul_2_Task_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Modul_2_Task_2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8450babd-08de-41a4-b4ca-c5c7b0e5bdb4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
